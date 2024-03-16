91160c0229b51680e76601e0fa7be2d3f8117bea
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-03-16 15:51:04 +0700

    style: fix layout card pengurus & blog

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,13 +8,16 @@
   <title>Sailor Bootstrap Template - Index</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
   <!-- Favicons -->
   <link href="{{url('assets/img/favicon.png')}}" rel="icon">
   <link href="{{url('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="{{ url('assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
@@ -38,25 +41,29 @@
 
       <!-- <h1 class="logo me-auto"><a href="#">Mentari</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="#" class="logo me-auto"><img src="https://mentarisehatindonesiakabdemak.com/public/assets/img/navbar-logo.png" alt="" class="img-fluid"></a>
+      <a href="#" class="logo me-auto"><img
+          src="https://mentarisehatindonesiakabdemak.com/public/assets/img/navbar-logo.png" alt=""
+          class="img-fluid"></a>
 
       @include('layouts.navbar')
-      
+
     </div>
   </header><!-- End Header -->
 
-    @yield('slider')
+  @yield('slider')
 
-  <main id="main">
+  <main id="main" style="min-height: 100vh; display: flex; flex-direction: column; justify-content: space-between;">
     @yield('content')
+
+    @include('layouts.footer')
   </main><!-- End #main -->
 
-  @include('layouts.footer')
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
   <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
